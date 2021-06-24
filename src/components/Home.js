005146d0892b45c39efba2fb5819b361304ddd49
Nitish <genitish@gmail.com>
2021-06-24 14:36:06 +0530

    styling done

--- src/components/Home.js
+++ src/components/Home.js
@@ -57,12 +57,16 @@ function Home() {
             <div>
                 {selectedCountry.country && 
                     <div className="cases-container">
-                        <h3>Country : {selectedCountry.country} </h3>
-                        <h3> Total Cases : {selectedCountry.cases} </h3>
-                        <h3> Active Cases : {selectedCountry.active} </h3>
-                        <h3> Recovered Cases : {selectedCountry.recovered} </h3>
-                        <h3> Deaths : {selectedCountry.deaths} </h3>
-                        <img src={selectedCountry.countryInfo.flag} />
+                        <div>
+                            <h2 className = "country">{selectedCountry.country} </h2>
+                            <h3 className = "total"> Total Cases : {selectedCountry.cases} </h3>
+                            <h3 className = "active"> Active Cases : {selectedCountry.active} </h3>
+                            <h3 className = "recovered"> Recovered Cases : {selectedCountry.recovered} </h3>
+                            <h3 className = "deaths"> Deaths : {selectedCountry.deaths} </h3>
+                        </div>
+                        <div>
+                            <img src={selectedCountry.countryInfo.flag} />
+                        </div>
                     </div>
                 }  
             </div>
